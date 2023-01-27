ffbc33c02c0c2aa8aee3f74211db5bad0da983e0
nno3onn <nno3onn@naver.com>
2023-01-27 18:36:55 +0900

    feat: 초기 세팅

--- src/frontend/src/index.tsx
+++ src/frontend/src/index.tsx
@@ -1,16 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import GlobalStyle from "./styles/globalStyle";
+import { ThemeProvider } from "styled-components";
+import theme from "./styles/theme";
+import { BrowserRouter } from "react-router-dom";
+import { HelmetProvider } from "react-helmet-async";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const queryClient = new QueryClient();
+
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <QueryClientProvider client={queryClient}>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <HelmetProvider>
+          <App />
+        </HelmetProvider>
+      </ThemeProvider>
+    </BrowserRouter>
+  </QueryClientProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/frontend/src/routes/router.tsx
@@ -0,0 +1,5 @@
+const Router = () => {
+  return <></>;
+};
+
+export default Router;
