a7b9e6d27cbebbfa2d29a8bfdd60dc15391c9d37
nno3onn <nno3onn@naver.com>
2023-01-28 00:33:25 +0900

    style: 테마 추가

--- src/frontend/src/styles/theme.ts
+++ src/frontend/src/styles/theme.ts
@@ -1,3 +1,38 @@
-const theme = {};
+const theme = {
+  fontSize: {
+    xs: ".75rem", // 12px
+    sm: ".875rem", // 14px
+    base: "1rem", // 16px
+    lg: "1.125rem", // 18px
+    xl: "1.25rem", // 20px
+    "2xl": "1.5rem", // 24px
+    "3xl": "2rem", // 32px
+  },
+  backgroundColor: {
+    primary: "#5a68ea",
+    "black-1": "#202225",
+    "black-2": "#18191c",
+    "grey-1": "#2f3136",
+    "grey-2": "#4f545c",
+    "grey-3": "#747f8d",
+    "grey-4": "#e3e5e8",
+    "green-1": "#2d7d46",
+    "greene-2": "#3ba55c",
+    orange: "#faa61a",
+    border: "#4f545c7a",
+  },
+  color: {
+    white: "#fff",
+    "black-1": "#060607",
+    "black-2": "#4f5660",
+    "grey-1": "#96989d",
+    "grey-2": "#a3a6aa",
+    "grey-3": "#b9bbbe",
+    "grey-4": "#dcddde",
+    "link-blue": "#4ca7e5",
+    red: "#ed4245",
+    "red-2": "#c74945",
+  },
+};
 
 export default theme;
