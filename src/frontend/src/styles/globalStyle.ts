46d054461665b5e98c9bab2a3922d6746e81a172
nno3onn <nno3onn@naver.com>
2023-01-28 00:35:08 +0900

    feat: gg sans 폰트 적용

--- src/frontend/src/styles/globalStyle.ts
+++ src/frontend/src/styles/globalStyle.ts
@@ -1,8 +1,25 @@
 import { createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
+import GGsansMedium from "../fonts/ggsansMedium.ttf";
+import GGsansBold from "../fonts/ggsansBold.ttf";
 
 const GlobalStyle = createGlobalStyle`
   ${reset}
+
+  @font-face {
+    font-family: 'gg sans Medium';
+    src: local('gg sans Medium'), local('ggsansMedium'), url(${GGsansMedium}) format('truetype');
+    font-style: normal;
+  }
+  @font-face {
+    font-family: 'gg sans Bold';
+    src: local('gg sans Bold'), local('ggsansBold'), url(${GGsansBold}) format('truetype');
+    font-style: bold;
+  }
+
+  html {
+    font-family: 'gg sans Medium';
+  }
 `;
 
 export default GlobalStyle;
