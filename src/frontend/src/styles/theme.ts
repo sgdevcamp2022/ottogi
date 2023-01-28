245b6f8e2a83e2df062368deaeac0c28f9e4296a
nno3onn <nno3onn@naver.com>
2023-01-28 12:57:27 +0900

    feat: 테마 타입 추가

--- src/frontend/src/styles/theme.ts
+++ src/frontend/src/styles/theme.ts
@@ -1,7 +1,65 @@
-const theme = {
+export type FontSizeType = "xs" | "sm" | "base" | "lg" | "xl" | "2xl" | "3xl";
+export type BackgroundColorType =
+  | "primary"
+  | "black-1"
+  | "black-2"
+  | "grey-1"
+  | "grey-2"
+  | "grey-3"
+  | "grey-4"
+  | "green-1"
+  | "green-2"
+  | "orange"
+  | "border";
+export type ColorType = "white" | "black-1" | "black-2" | "grey-1" | "grey-2" | "grey-3" | "grey-4" | "blue" | "red-1" | "red-2";
+
+interface FontSize {
+  xs: string;
+  sm: string;
+  base: string;
+  lg: string;
+  xl: string;
+  "2xl": string;
+  "3xl": string;
+}
+
+interface BackgroundColor {
+  primary: string;
+  "black-1": string;
+  "black-2": string;
+  "grey-1": string;
+  "grey-2": string;
+  "grey-3": string;
+  "grey-4": string;
+  "green-1": string;
+  "green-2": string;
+  orange: string;
+  border: string;
+}
+
+interface Color {
+  white: string;
+  "black-1": string;
+  "black-2": string;
+  "grey-1": string;
+  "grey-2": string;
+  "grey-3": string;
+  "grey-4": string;
+  blue: string;
+  "red-1": string;
+  "red-2": string;
+}
+
+interface ThemeType {
+  fontSize: FontSize;
+  backgroundColor: BackgroundColor;
+  color: Color;
+}
+
+const theme: ThemeType = {
   fontSize: {
-    xs: ".75rem", // 12px
-    sm: ".875rem", // 14px
+    xs: "0.75rem", // 12px
+    sm: "0.875rem", // 14px
     base: "1rem", // 16px
     lg: "1.125rem", // 18px
     xl: "1.25rem", // 20px
@@ -16,8 +74,8 @@ const theme = {
     "grey-2": "#4f545c",
     "grey-3": "#747f8d",
     "grey-4": "#e3e5e8",
-    "green-1": "#2d7d46",
-    "greene-2": "#3ba55c",
+    "green-1": "#447b4c",
+    "green-2": "#3ba55c",
     orange: "#faa61a",
     border: "#4f545c7a",
   },
@@ -29,8 +87,8 @@ const theme = {
     "grey-2": "#a3a6aa",
     "grey-3": "#b9bbbe",
     "grey-4": "#dcddde",
-    "link-blue": "#4ca7e5",
-    red: "#ed4245",
+    blue: "#4ca7e5",
+    "red-1": "#ed4245",
     "red-2": "#c74945",
   },
 };
