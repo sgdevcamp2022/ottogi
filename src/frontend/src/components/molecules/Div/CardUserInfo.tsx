7284283671e8de27479cf1009b39ad9cbb20952f
김현우 <krokerdile@naver.com>
2023-02-05 13:32:20 +0900

    feat: 2/1~2/5 작업 molecules - LogoUpload - CardUserInfo - FieldList - ServerInput - ServerText

---
+++ src/frontend/src/components/molecules/Div/CardUserInfo.tsx
@@ -0,0 +1,66 @@
+import styled from "styled-components";
+import FieldButton from "../../atoms/Button/fieldButton";
+import Text from "../../atoms/Text/Text";
+
+const CardUserInfo = () => {
+  return (
+    <InfoContainer>
+      <Logo />
+      <NameWrapper>
+        <Text text="UserName" fontSize="lg" fontWeight="bold" color="white" />
+        <Text
+          text="UserName"
+          fontSize="lg"
+          fontWeight="bold"
+          color="setting-tab"
+        />
+      </NameWrapper>
+      <>
+        <ButtonWrappper>
+          <FieldButton
+            text="사용자 프로필 편집"
+            onClick={() => console.log(1)}
+          />
+        </ButtonWrappper>
+      </>
+    </InfoContainer>
+  );
+};
+
+export default CardUserInfo;
+
+const Logo = styled.div`
+  width: 80px;
+  height: 80px;
+  background-color: yellow;
+  position: absolute;
+  top: 82px;
+  left: 22px;
+  -webkit-box-sizing: content-box;
+  box-sizing: content-box;
+  border-radius: 3rem;
+  border: 0.125rem solid white;
+`;
+
+const InfoContainer = styled.div`
+  height: 72px;
+  width: 100%;
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-pack: justify;
+  -ms-flex-pack: justify;
+  justify-content: space-between;
+  padding: 16px 16px 0 120px;
+  background-color: ${({ theme }) => theme.backgroundColor["voice-nobody"]};
+`;
+
+const ButtonWrappper = styled.div`
+  width: auto;
+  height: 2rem;
+`;
+
+const NameWrapper = styled.div`
+  width: auto;
+  display: flex;
+`;
