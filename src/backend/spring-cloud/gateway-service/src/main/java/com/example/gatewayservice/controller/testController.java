e68151191ddaa52d545cbfae6627bc7541b0cd7c
whipbaek <jibaek0513@gmail.com>
2023-02-12 20:01:52 +0900

    feat: 테스트 컨트롤러 작성

--- src/backend/spring-cloud/gateway-service/src/main/java/com/example/gatewayservice/controller/testController.java
+++ src/backend/spring-cloud/gateway-service/src/main/java/com/example/gatewayservice/controller/testController.java
@@ -1,2 +1,14 @@
-package com.example.gatewayservice.controller;public class testController {
+package com.example.gatewayservice.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class testController {
+
+    @GetMapping("/test")
+    public String test(){
+        return "게이트웨이 서버 테스트 성공";
+    }
 }
