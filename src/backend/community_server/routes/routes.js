9c25aa6c45b68bdf0ae3814737a0cac6cf4e8f13
freemoon99 <soloist132@gmail.com>
2023-02-18 03:01:10 +0900

    feat: 이미지 업로드 기능 추가
    -이미지 업로드 기능 추가(s3업로드)

--- src/backend/community_server/routes/routes.js
+++ src/backend/community_server/routes/routes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const {upload} = require('../confing/s3');
 const category = require('../controllers/categoryController');
 const channel = require('../controllers/channelController');
 const community = require('../controllers/communityController');
@@ -8,21 +9,20 @@ router.get('/test', (req, res) => {
     res.send('good connect!');
 })
 
+//커뮤니티 하위 카테고리, 채널 조회 -> json 형식으로 보내주기
+router.get('/community/getlist', community.loadList);
+router.get('/community/getoption', category.loadList);
+
 //커뮤니티(서버) 생성
-router.post('/community/create', community.communityCreate);
+router.post('/community/create', upload.single('img'), community.communityCreate);
 //커뮤니티 참가
 router.post('/community/join', community.communityJoin);
 //커뮤니티 정보 변경(이름)
-
 router.patch('/community/update', community.communityRename);
 //커뮤니티 정보 변경(프로필)
 router.patch('/community/profile', community.updateProfile);
 //커뮤니티 삭제 
 router.delete('/community/delete', community.communityDelete);
-
-//커뮤니티 하위 카테고리, 채널 조회 -> json 형식으로 보내주기
-router.get('/community/getlist', community.loadList);
-router.get('/community/getoption', category.loadList);
 
 // 카테고리 생성 
 router.post('/community/category/create', category.categoryCreate);
