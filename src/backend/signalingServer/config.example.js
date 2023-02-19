74af07e9a4e6180e3ad3cd68b14c03dfcbc163bb
Kimsc9976 <tncks097@naver.com>
2023-02-19 11:54:06 +0900

    Feat: config 추가

--- src/backend/signalingServer/config.example.js
+++ src/backend/signalingServer/config.example.js
@@ -1,65 +1,161 @@
+const os = require('os');
 module.exports = {
-  socketId : undefined,
-  listenIp: '0.0.0.0',
-  listenPort: 3000,
-  // sslCrt: '/etc/ssl/certs/ssl-cert-snakeoil.pem',
-  // sslKey: '/etc/ssl/private/ssl-cert-snakeoil.key',
-  cert : process.env.HTTPS_CERT_FULLCHAIN || `${__dirname}/server/certs/cert.pem`, // have to change for get `${__dirname}/server/certs/fullchain.pem`,
-  key  : process.env.HTTPS_CERT_PRIVKEY   || `${__dirname}/server/certs/key.pem`,  // have to change for get `${__dirname}/server/certs/privkey.pem`,
-  mediasoup: {
-    // Worker settings
-    worker: {
-      rtcMinPort: 10000,
-      rtcMaxPort: 10100,
-      logLevel: 'warn',
-      logTags: [
-        'info',
-        'ice',
-        'dtls',
-        'rtp',
-        'srtp',
-        'rtcp',
-        // 'rtx',
-        // 'bwe',
-        // 'score',
-        // 'simulcast',
-        // 'svc'
-      ],
-    },
-    // Router settings
-    router: {
-      mediaCodecs:
-        [
-          {
-            kind: 'audio',
-            mimeType: 'audio/opus',
-            clockRate: 48000,
-            channels: 2
-          },
-          {
-            kind: 'video',
-            mimeType: 'video/VP8',
-            clockRate: 90000,
-            parameters:
-              {
-                'x-google-start-bitrate': 1000
-              }
-          },
-        ]
-    },
-    // WebRtcTransport settings
-    webRtcTransport: {
-      listenIps: [
-        {
-          ip: '127.0.0.1',
-          announcedIp: null,
-          // 제가 사용한 기본 default값 입니다. 
-          // ip: '0.0.0.0', // server side ip which have to change
-          // announcedIp: '127.0.0.1', // // this is our hostmachine
-        }
-      ],
-      maxIncomingBitrate: 1500000,
-      initialAvailableOutgoingBitrate: 1000000,
-    }
-  }
+	domain : process.env.DOMAIN || 'localhost',
+	// Signaling settings (protoo WebSocket server and HTTP API server).
+	https  :
+	{
+		listenIp   : '0.0.0.0',
+		// NOTE: Don't change listenPort (client app assumes 4443).
+		listenPort : process.env.PROTOO_LISTEN_PORT || 4443,
+		// NOTE: Set your own valid certificate files.
+		tls        :
+		{
+      cert : process.env.HTTPS_CERT_FULLCHAIN || `${__dirname}/server/certs/cert.pem`, // have to change for get `${__dirname}/server/certs/fullchain.pem`,
+      key  : process.env.HTTPS_CERT_PRIVKEY ||  `${__dirname}/server/certs/key.pem`,   // have to change for get`${__dirname}/server/certs/privkey.pem`,
+		}
+	},
+  mediasoup :
+	{
+		// Number of mediasoup workers to launch.
+		numWorkers     : Object.keys(os.cpus()).length,
+		// mediasoup WorkerSettings.
+		// See https://mediasoup.org/documentation/v3/mediasoup/api/#WorkerSettings
+		workerSettings :
+		{
+			logLevel : 'warn',
+			logTags  :
+			[
+				'info',
+				'ice',
+				'dtls',
+				'rtp',
+				'srtp',
+				'rtcp',
+				'rtx',
+				'bwe',
+				'score',
+				'simulcast',
+				'svc',
+				'sctp'
+			],
+			rtcMinPort : process.env.MEDIASOUP_MIN_PORT || 40000,
+			rtcMaxPort : process.env.MEDIASOUP_MAX_PORT || 49999
+		},
+		// mediasoup Router options.
+		// See https://mediasoup.org/documentation/v3/mediasoup/api/#RouterOptions
+		routerOptions :
+		{
+			mediaCodecs :
+			[
+				{
+					kind      : 'audio',
+					mimeType  : 'audio/opus',
+					clockRate : 48000,
+					channels  : 2
+				},
+				{
+					kind       : 'video',
+					mimeType   : 'video/VP8',
+					clockRate  : 90000,
+					parameters :
+					{
+						'x-google-start-bitrate' : 1000
+					}
+				},
+				{
+					kind       : 'video',
+					mimeType   : 'video/VP9',
+					clockRate  : 90000,
+					parameters :
+					{
+						'profile-id'             : 2,
+						'x-google-start-bitrate' : 1000
+					}
+				},
+				{
+					kind       : 'video',
+					mimeType   : 'video/h264',
+					clockRate  : 90000,
+					parameters :
+					{
+						'packetization-mode'      : 1,
+						'profile-level-id'        : '4d0032',
+						'level-asymmetry-allowed' : 1,
+						'x-google-start-bitrate'  : 1000
+					}
+				},
+				{
+					kind       : 'video',
+					mimeType   : 'video/h264',
+					clockRate  : 90000,
+					parameters :
+					{
+						'packetization-mode'      : 1,
+						'profile-level-id'        : '42e01f',
+						'level-asymmetry-allowed' : 1,
+						'x-google-start-bitrate'  : 1000
+					}
+				}
+			]
+		},
+		// mediasoup WebRtcServer options for WebRTC endpoints (mediasoup-client,
+		// libmediasoupclient).
+		// See https://mediasoup.org/documentation/v3/mediasoup/api/#WebRtcServerOptions
+		// NOTE: mediasoup-demo/server/lib/Room.js will increase this port for
+		// each mediasoup Worker since each Worker is a separate process.
+		webRtcServerOptions :
+		{
+			listenInfos :
+			[
+				{
+					protocol    : 'udp',
+					ip          : process.env.MEDIASOUP_LISTEN_IP || '0.0.0.0',
+					announcedIp : process.env.MEDIASOUP_ANNOUNCED_IP,
+					port        : 44444
+				},
+				{
+					protocol    : 'tcp',
+					ip          : process.env.MEDIASOUP_LISTEN_IP || '0.0.0.0',
+					announcedIp : process.env.MEDIASOUP_ANNOUNCED_IP,
+					port        : 44444
+				}
+			],
+		},
+		// mediasoup WebRtcTransport options for WebRTC endpoints (mediasoup-client,
+		// libmediasoupclient).
+		// See https://mediasoup.org/documentation/v3/mediasoup/api/#WebRtcTransportOptions
+		webRtcTransportOptions :
+		{
+			// listenIps is not needed since webRtcServer is used.
+			// However passing MEDIASOUP_USE_WEBRTC_SERVER=false will change it.
+			listenIps :
+			[
+				{
+					ip          : process.env.MEDIASOUP_LISTEN_IP || '0.0.0.0',
+					announcedIp : process.env.MEDIASOUP_ANNOUNCED_IP || '127.0.0.1'// || ANNOUNCED IP
+				}
+			],
+			initialAvailableOutgoingBitrate : 1000000,
+			minimumAvailableOutgoingBitrate : 600000,
+			maxSctpMessageSize              : 262144,
+			// Additional options that are not part of WebRtcTransportOptions.
+			maxIncomingBitrate              : 1500000,
+			// enableUdp : true,
+      // enableTcp : true,
+      // preferUdp : true,  // have to choose prefer UDP or prefer TCP
+      // preferTcp : false, // have to choose prefer TCP or prefer UDP
+		},
+		// mediasoup PlainTransport options for legacy RTP endpoints (FFmpeg,
+		// GStreamer).
+		// See https://mediasoup.org/documentation/v3/mediasoup/api/#PlainTransportOptions
+		plainTransportOptions :
+		{
+			listenIp :
+			{
+				ip          : process.env.MEDIASOUP_LISTEN_IP || '0.0.0.0',
+				announcedIp : process.env.MEDIASOUP_ANNOUNCED_IP
+			},
+			maxSctpMessageSize : 262144
+		}
+	}
 };
