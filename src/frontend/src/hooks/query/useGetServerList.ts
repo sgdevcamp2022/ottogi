d2683012a555e3d7d279093b4e4ede0c7a314a1c
nno3onn <nno3onn@naver.com>
2023-02-20 17:56:00 +0900

    feat: refresh ServerList

--- src/frontend/src/hooks/query/useGetServerList.ts
+++ src/frontend/src/hooks/query/useGetServerList.ts
@@ -1,8 +1,13 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import serverSettingApi from "@api/server";
 
 const useGetServerList = ({ userId }: any) => {
-  return useQuery(["ServerList", { userId }], serverSettingApi.getList);
+  const queryClient = useQueryClient();
+  return useQuery(["ServerList", { userId }], serverSettingApi.getList, {
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["ServerList"] });
+    },
+  });
 };
 
 export default useGetServerList;
