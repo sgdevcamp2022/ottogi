76909d965e5017dcb2f3e43ce14de2ad2ecf4503
nno3onn <nno3onn@naver.com>
2023-02-21 15:00:28 +0900

    fix: 유저 정보 불러오기 및 ServerList 오류 수정

--- src/frontend/src/hooks/query/useGetServerList.ts
+++ src/frontend/src/hooks/query/useGetServerList.ts
@@ -1,13 +1,8 @@
-import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 import serverSettingApi from "@api/server";
 
 const useGetServerList = ({ userId }: any) => {
-  const queryClient = useQueryClient();
-  return useQuery(["ServerList", { userId }], serverSettingApi.getList, {
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ["ServerList"] });
-    },
-  });
+  return useQuery(["ServerList", { userId }], serverSettingApi.getList);
 };
 
 export default useGetServerList;
