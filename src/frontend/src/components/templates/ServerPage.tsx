507428296ada20adb59e66d3359412c7bc0d44be
김현우 <krokerdile@naver.com>
2023-02-21 14:25:47 +0900

    fix: 메인페이지 들어오자마자 설정페이지 열리는 에러 해결

--- src/frontend/src/components/templates/ServerPage.tsx
+++ src/frontend/src/components/templates/ServerPage.tsx
@@ -1,6 +1,7 @@
 import UserSetting from "@pages/UserSetting";
 import useModalStore from "@store/useModalStore";
 import useUserSettingModalStore from "@store/useUserSettingModalStore";
+import { useEffect } from "react";
 import styled from "styled-components";
 import InviteFriendModal from "../organisms/InviteFriendModal";
 import ServerList from "../organisms/ServerList";
@@ -11,8 +12,12 @@ import Tab3ServerBody from "../organisms/Tab3ServerBody";
 import Tab3ServerHeader from "../organisms/Tab3ServerHeader";
 
 const ServerPage = () => {
-  const { inviteFriendModal } = useModalStore();
-  const { userSettingModal } = useUserSettingModalStore();
+  const { inviteFriendModal, setInviteFriendModal } = useModalStore();
+  const { userSettingModal, setUserSettingModal } = useUserSettingModalStore();
+  useEffect(() => {
+    setInviteFriendModal(false);
+    setUserSettingModal(false);
+  }, []);
   return (
     <>
       <ServerList />
